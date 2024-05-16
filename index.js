0e5e3740344bdd044dc6dad121745ddd5ae15cb2
NISHANT <thakurnishant0420@gmail.com>
2024-05-16 15:15:55 +0530

    working with for,while,doWhile loop & infinite loop

--- index.js
+++ index.js
@@ -2,18 +2,57 @@ console.log('Hello World!')
 let a=5
 console.log(a++)
 console.log(a)
+console.log()
 
 
-let n=prompt('your score is:')
-if(n>=90 && n<=100){
-    alert('A Grade') 
-}else if(n>=70 && n<90){
-    alert('B Grade')
-}else if(n>=60 && n<70){
-    alert('C Grade')
-}else if(n>=50 && n<60){
-    alert('D Grade')
+// let n=prompt('your score is:')
+// if(n>=90 && n<=100){
+//     alert('A Grade') 
+// }else if(n>=70 && n<90){
+//     alert('B Grade')
+// }else if(n>=60 && n<70){
+//     alert('C Grade')
+// }else if(n>=50 && n<60){
+//     alert('D Grade')
+// }
+// else{
+//     alert('F Grade')
+// }
+
+
+//for loop
+for(let i=1;i<=5;++i){
+    console.log("weed")
 }
-else{
-    alert('F Grade')
+console.log()
+// let n=prompt('Enter a number to get sum from 1 to n:')
+// let sum=0
+// for(let i=1;i<=n;++i){
+//     sum=sum+i
+// }
+// console.log(sum)
+
+//infinite loop
+// for(let i=1;i>0;i++){
+//     console.log("weed")
+// }
+
+//while loop
+let i=1
+while(i<=5){
+    console.log('weed')
+    i++
 }
+console.log()
+
+// do while loop
+let j=1
+do{
+    console.log('weed')
+    j++}while(j<=5)
+
+
+
+
+
+
