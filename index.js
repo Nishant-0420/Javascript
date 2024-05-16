d514bf50fbe25d061330240556d1488adcdfea70
NISHANT <thakurnishant0420@gmail.com>
2024-05-16 18:33:15 +0530

    practice js loop questions

--- index.js
+++ index.js
@@ -5,37 +5,37 @@ console.log(a)
 console.log()
 
 
-let n=prompt('your score is:')
-if(n>=90 && n<=100){
-    alert('A Grade') 
-}else if(n>=70 && n<90){
-    alert('B Grade')
-}else if(n>=60 && n<70){
-    alert('C Grade')
-}else if(n>=50 && n<60){
-    alert('D Grade')
-}
-else{
-    alert('F Grade')
-}
+// let n=prompt('your score is:')
+// if(n>=90 && n<=100){
+//     alert('A Grade') 
+// }else if(n>=70 && n<90){
+//     alert('B Grade')
+// }else if(n>=60 && n<70){
+//     alert('C Grade')
+// }else if(n>=50 && n<60){
+//     alert('D Grade')
+// }
+// else{
+//     alert('F Grade')
+// }
 
 
 //for loop
-for(let i=1;i<=5;++i){
-    console.log("weed")
-}
-console.log()
-let n=prompt('Enter a number to get sum from 1 to n:')
-let sum=0
-for(let i=1;i<=n;++i){
-    sum=sum+i
-}
-console.log(sum)
+// for(let i=1;i<=5;++i){
+//     console.log("weed")
+// }
+// console.log()
+// let m=prompt('Enter a number to get sum from 1 to n:')
+// let sum=0
+// for(let i=1;i<=m;++i){
+//     sum=sum+i
+// }
+// console.log(sum)
 
-infinite loop
-for(let i=1;i>0;i++){
-    console.log("weed")
-}
+// infinite loop
+// for(let i=1;i>0;i++){
+//     console.log("weed")
+// }
 
 //while loop
 let i=1
@@ -79,6 +79,33 @@ for(let i in student){
     console.log(i,student.i)
 }
 
+console.log()
+// loop questions practice
+//print odd, even no. from 0-100
+let sum=0
+for(let i=0;i<=100;i++){
+    if(i%2===0){
+        console.log(i)
+        sum=sum+i
+    }
+}
+for(let i=1;i<=100;i+=2){
+        console.log(i)
+}
+// guess the game number 
+let gameNumber='69' 
+// if gameNumber =69 then use == and != as prompt converts the value to string
+let guess=prompt('Enter ur guess number between 1-100:')
+if(gameNumber===guess){
+    alert('Congratulations! You have guessed the number')}
+    else{
+        alert('Try again!!!!!')}
+// use while loop kyuki datatype ki dikat nhi aega
+while(gameNumber!==guess){
+    guess=prompt('Enter ur guess number between 1-100:')
+}
+alert('Congratulations! You have guessed the number')
+    
 
 
 
